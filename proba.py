1a5cba434fdd391d0ceb6d279148356d513523d0
GerValle <GerValle@users.noreply.github.com>
2022-11-09 15:16:07 -0600

    No se de que se trato este pero ahi va...

---
+++ proba.py
@@ -0,0 +1,15 @@
+#%% 
+
+import yfinance as yf
+
+
+symbols = ["AAPL", "TWTR", "MSFT"]
+
+
+Tickers = yf.Tickers(symbols)
+
+history = Tickers.history('500d')['Close']
+
+returns = history.pct_change()
+
+returns.plot()
